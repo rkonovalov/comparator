28328e5c45edc76457d0734e783143732dd1a6fd
rkonovalov <rkonovalov86@gmail.com>
2019-05-03 12:19:16 -0400

    Changed parameters name

--- src/main/java/com/comparator/Comparator.java
+++ src/main/java/com/comparator/Comparator.java
@@ -39,13 +39,13 @@ public class Comparator<T, K> {
      * Returns an {@code Comparator} with the specified present nullable value.
      *
      * @param value             value of nullable object
-     * @param returnObjectClass class of object which will be returned in {@code get()} method
+     * @param resultObjectClass class of object which will be returned in {@code get()} method
      * @param <T>               Comparable object
      * @param <U>               type of object
      * @return an {@code Comparator} with the value present
      */
-    public static <T, U> Comparator<T, U> of(T value, Class<U> returnObjectClass) {
-        if (returnObjectClass == null)
+    public static <T, U> Comparator<T, U> of(T value, Class<U> resultObjectClass) {
+        if (resultObjectClass == null)
             throw new NoSuchElementException("returnObjectClass couldn't be null");
         return new Comparator<>(value);
     }
@@ -54,19 +54,19 @@ public class Comparator<T, K> {
      * Adds condition and return function in items list
      *
      * @param condition   a condition to apply to the value, if present
-     * @param resultValue a mapping function to apply to the value, if present
+     * @param resultExpression a mapping function to apply to the value, if condition is present
      * @return an {@code Comparator} with the value present
      * @throws NoSuchElementException if condition or mapping function is null
      */
     @SuppressWarnings("unchecked")
-    public Comparator<T, K> compare(Predicate<? super T> condition, Function<T, K> resultValue) {
+    public Comparator<T, K> compare(Predicate<? super T> condition, Function<T, K> resultExpression) {
 
         if (condition == null) {
             throw new NoSuchElementException("No condition present");
-        } else if (resultValue == null)
+        } else if (resultExpression == null)
             throw new NoSuchElementException("No mapping function present");
 
-        items.add(new ComparatorItem(value, condition, resultValue));
+        items.add(new ComparatorItem(value, condition, resultExpression));
         return this;
     }
 
@@ -74,17 +74,17 @@ public class Comparator<T, K> {
      * Adds {@code expectedValue} and return function in items list
      *
      * @param expectedValue expected value of object
-     * @param resultValue   a mapping function to apply to the value, if present
+     * @param resultExpression   a mapping function to apply to the value, if expectedValue is present
      * @param <U>           The type of the result of the mapping function
      * @return an {@code Comparator} with the value present
      * @throws NoSuchElementException if mapping function is null
      */
     @SuppressWarnings("unchecked")
-    public <U> Comparator<T, K> compare(Object expectedValue, Function<? super T, ? extends U> resultValue) {
-        if (resultValue == null)
+    public <U> Comparator<T, K> compare(Object expectedValue, Function<? super T, ? extends U> resultExpression) {
+        if (resultExpression == null)
             throw new NoSuchElementException("No mapping function present");
 
-        items.add(new ComparatorItem(value, (s -> s.equals(expectedValue)), resultValue));
+        items.add(new ComparatorItem(value, (s -> s.equals(expectedValue)), resultExpression));
         return this;
     }
 
@@ -92,12 +92,12 @@ public class Comparator<T, K> {
      * Adds {@code expectedValue} and return function in items list
      *
      * @param expectedValue expected value of object
-     * @param resultObject  result value of object
+     * @param resultValue  result value of object if expectedValue is present
      * @return an {@code Comparator} with the value present
      */
     @SuppressWarnings("unchecked")
-    public Comparator<T, K> compare(Object expectedValue, Object resultObject) {
-        items.add(new ComparatorItem(value, (s -> s.equals(expectedValue)), (s -> resultObject)));
+    public Comparator<T, K> compare(Object expectedValue, Object resultValue) {
+        items.add(new ComparatorItem(value, (s -> s.equals(expectedValue)), (s -> resultValue)));
         return this;
     }
 
@@ -120,7 +120,7 @@ public class Comparator<T, K> {
             Optional<T> optional = (Optional<T>) Optional.ofNullable(item.getValue());
 
             Object obj = optional.filter(item.getCondition())
-                    .map(item.getMapper())
+                    .map(item.getResultExpression())
                     .orElse(null);
             if (obj != null)
                 return obj;
